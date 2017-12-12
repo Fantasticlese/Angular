1759edcb8b0a3ea41ead564b6e52c8ef684609e7
Fantasticlese <joshuathorup@gmail.com>
2017-12-12 10:14:32 -0700

    Created an Angular 2 service for managing API data

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -16,21 +16,21 @@ var DocumentsComponent = /** @class */ (function () {
                 description: "It's very round",
                 file_url: 'http://google.com',
                 updated_at: '12/8/2017',
-                image_url: 'http://google.com',
+                image_url: 'https://theopenmic.co/wp-content/uploads/2016/01/freelancer.jpg',
             },
             {
                 title: "My Second Doc",
                 description: "It's very round",
                 file_url: 'http://google.com',
                 updated_at: '12/8/2017',
-                image_url: 'http://google.com',
+                image_url: 'http://echeck.org/wp-content/uploads/2017/10/Best-And-Cheapest-Ways-to-Pay-Freelancers-Online-cover-image.png',
             },
             {
                 title: "My Last Doc",
                 description: "It's very round",
                 file_url: 'http://google.com',
                 updated_at: '12/8/2017',
-                image_url: 'http://google.com',
+                image_url: 'https://cdn.searchenginejournal.com/wp-content/uploads/2016/04/freelancersecrets.jpg',
             }
         ];
     }
@@ -38,7 +38,8 @@ var DocumentsComponent = /** @class */ (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'documents',
-            templateUrl: 'documents.component.html'
+            templateUrl: 'documents.component.html',
+            styleUrls: ['documents.component.css']
         })
     ], DocumentsComponent);
     return DocumentsComponent;
--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -4,7 +4,8 @@ import { Document } from './document';
 @Component({
 	moduleId: module.id,
 	selector: 'documents',
-	templateUrl: 'documents.component.html'
+	templateUrl: 'documents.component.html',
+	styleUrls: ['documents.component.css']
 })
 
 export class DocumentsComponent {
@@ -16,21 +17,21 @@ export class DocumentsComponent {
 			description: "It's very round",
 			file_url: 'http://google.com',
 			updated_at: '12/8/2017',
-			image_url: 'http://google.com',
+			image_url: 'https://theopenmic.co/wp-content/uploads/2016/01/freelancer.jpg',
 		},
 		{
 			title: "My Second Doc",
 			description: "It's very round",
 			file_url: 'http://google.com',
 			updated_at: '12/8/2017',
-			image_url: 'http://google.com',
+			image_url: 'http://echeck.org/wp-content/uploads/2017/10/Best-And-Cheapest-Ways-to-Pay-Freelancers-Online-cover-image.png',
 		},
 		{
 			title: "My Last Doc",
 			description: "It's very round",
 			file_url: 'http://google.com',
 			updated_at: '12/8/2017',
-			image_url: 'http://google.com',
+			image_url: 'https://cdn.searchenginejournal.com/wp-content/uploads/2016/04/freelancersecrets.jpg',
 		}
 	]
 }
